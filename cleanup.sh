3249a00d4c7d55f4dcd220bcca8887ded624da23
ivanp7 <ivanp7@mail.ru>
2022-11-01 15:16:07 +0300

    fix: make this work from dot-files/installer

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd -- "$(dirname -- "$0")"
+cd -- "$(dirname -- "$0")/.."
 
 rm -fv PKGBUILD
 rm -fv dot-files.install
--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 cd -- "$(dirname -- "$0")"
 
-cp -f PKGBUILD.template PKGBUILD
-cp -f dot-files.install.template dot-files.install
+cp -f PKGBUILD.template ../PKGBUILD
+cp -f dot-files.install.template ../dot-files.install
+
+cd ..
 
 : ${PACKAGE_POSTFIX:="$(git -C . rev-parse --abbrev-ref HEAD 2> /dev/null)"}
 : ${PACKAGE_POSTFIX:="custom"}
